fd2ec0c19985147318e5fdae16943b1058c93396
langxue <langxue@abrain.com.cn>
2022-06-02 16:53:05 +0800

    询问用户是否覆盖原文件

--- bin/main.js
+++ bin/main.js
@@ -10,6 +10,9 @@ console.log('langxue-cli working~')
 
 
 const inquirer = require('inquirer')
+// const chalk = require('chalk')
+const figlet = require('figlet');
+
 
 inquirer.prompt([
   {
@@ -26,6 +29,7 @@ inquirer.prompt([
 
 
 const program = require('commander')
+
 program
   // 定义命令和参数
   .command('create <app-name>')
@@ -34,13 +38,45 @@ program
   .option('-f, --force', 'overwrite target directory if it exist')
   .action((name, options) => {
     // 打印执行结果
-    console.log('name:',name,'options:',options)
+    // console.log('name:',name,'options:',options)
+
+    // 文本样式
+    console.log("project name is " + name)
+
+    // 颜色
+    // console.log("project name is " + chalk.cyan(name))
+    // console.log("project name is " + chalk.green(name))
+
+    // // 背景色
+    // console.log("project name is " + chalk.bgRed(name))
+
+    // // 使用RGB颜色输出
+    // console.log("project name is " + chalk.rgb(4, 156, 219).underline(name));
+    // console.log("project name is " + chalk.hex('#049CDB').bold(name));
+    // console.log("project name is " + chalk.bgHex('#049CDB').bold(name))
+
+    require('../lib/create.js')(name, options)
   })
   
 program
    // 配置版本号信息
   .version(`v${require('../package.json').version}`)
   .usage('<command> [option]')
+
+program
+  // 监听 --help 执行
+  .on('--help', () => {
+    // 使用 figlet 绘制 Logo
+    console.log('\r\n' + figlet.textSync('langxue', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 80,
+      whitespaceBreak: true
+    }));
+    // 新增说明信息
+    console.log(`\r\nRun ${chalk.cyan(`lx <command> --help`)} for detailed usage of given command\r\n`)
+  })
   
 // 解析用户执行命令传入参数
 program.parse(process.argv);
